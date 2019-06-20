06f0142335ad68372e26e2aa824e6c43ce2147b7
cildhdi <miles_yang@foxmail.com>
2019-06-20 21:30:32 +0800

    add login

---
+++ config.js
@@ -0,0 +1,5 @@
+const mainURL = "https://yxm.cildhdi.cn/api/mp/";
+
+export const loginUrl = mainURL + 'login';
+export const getUserInfoUrl = mainURL + 'userinfo';
+export const setUserInfoUrl = mainURL + 'setuserinfo';
---
+++ pages/info/info.js
@@ -0,0 +1,73 @@
+// info.js
+import * as config from './../../config.js'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: '',
+    schoolId: ''
+  },
+
+  idChange(e) {
+    this.setData({
+      schoolId: e.detail.detail.value
+    });
+  },
+  nameChange(e) {
+    this.setData({
+      name: e.detail.detail.value
+    });
+  },
+
+  confirm() {
+    if (this.data.name.length == 0 || this.data.schoolId.length == 0) {
+      wx.showToast({
+        title: '请输入非空信息',
+        icon: 'none'
+      });
+      return;
+    }
+    wx.showLoading({
+      title: '信息上传中',
+    })
+    wx.request({
+      url: config.setUserInfoUrl,
+      method: 'POST',
+      data: {
+        Token: wx.getStorageSync("Token"),
+        Name: this.data.name,
+        SchoolID: this.data.schoolId
+      },
+      success: (res) => {
+        wx.hideLoading();
+        if (res.statusCode == 200) {
+          wx.setStorageSync("Name", this.data.name);
+          wx.setStorageSync("SchoolId", this.data.schoolId);
+          wx.navigateBack({
+            delta: 1
+          });
+        }
+      },
+      fail: () => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '上传失败，请稍后再试',
+          icon: 'none'
+        })
+      }
+    });
+  },
+
+  onUnload: () => {
+    var name = wx.getStorageSync("Name");
+    var id = wx.getStorageSync("SchoolId");
+    if (!(name && name.length != 0 && id && id.length != 0)) {
+      wx.navigateTo({
+        url: '../../pages/info/info',
+      });
+    }
+  }
+})

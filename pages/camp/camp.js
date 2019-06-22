afea53c8529bc00872c57093f9de0568a35f0e5d
zeroBe0 <n_liangbing@163.com>
2019-06-22 16:24:14 +0800

    change camp page

--- pages/camp/camp.js
+++ pages/camp/camp.js
@@ -63,6 +63,23 @@ Page({
   onShareAppMessage: function () {
 
   },
+  cet4bt:function(){
+    wx.navigateTo({
+      url: '../cet4/cet4',
+    })
+  },
+  cet6bt: function () {
+    wx.navigateTo({
+      url: '../cet6/cet6',
+    })
+  },
+  Hbt: function(){
+    wx.showToast({
+      title: '休息一下吧',
+      icon: 'loading',
+      duration: 5000,
+    })
+  }
 
 
 
